98c95c5273ece1ef2cd900e8360793def94cb837
MichalLukac <cospelthetraceur@gmail.com>
2019-09-25 13:44:53 +0200

    Update long numbers

--- CRNN/augmentor/long_numbers.py
+++ CRNN/augmentor/long_numbers.py
@@ -8,7 +8,7 @@ class MyAugmentor(object):
         sometimes = lambda aug: iaa.Sometimes(0.5, aug)
         self.seq = iaa.Sequential([
             iaa.Multiply((0.8, 1.2), per_channel=0.5),
-            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.05))),
+            #sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.05))),
             sometimes(
                 iaa.OneOf([
                     iaa.CoarseDropout((0.01, 0.03), size_percent=(0.1, 0.3)),
@@ -17,7 +17,7 @@ class MyAugmentor(object):
                     iaa.Salt((0.03,0.05))
                 ])
             ),
-            iaa.Multiply((0.8, 1.2), per_channel=0.5),
+            #iaa.Multiply((0.8, 1.2), per_channel=0.5),
             sometimes(iaa.FrequencyNoiseAlpha(
                     exponent=(-4, 0),
                     first=iaa.Multiply((0.8, 1.2), per_channel=0.5),
@@ -32,15 +32,15 @@ class MyAugmentor(object):
                     iaa.MedianBlur(k=(3, 5))
                 ])
             ),
-            sometimes(
-                iaa.CropAndPad(
-                    percent=(-0.05, 0.1),
-                    pad_mode='constant',
-                    pad_cval=(0, 255)
-                ),
-            ),
-            sometimes(iaa.ElasticTransformation(alpha=(1.0, 2.0), sigma=(2.0, 3.0))), # move pixels locally around (with random strengths)
-            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02), mode='constant')), # sometimes move parts of the image around
+            #sometimes(
+            #    iaa.CropAndPad(
+            #        percent=(-0.05, 0.1),
+            #        pad_mode='constant',
+            #        pad_cval=(0, 255)
+            #    ),
+            #),
+            #sometimes(iaa.ElasticTransformation(alpha=(1.0, 2.0), sigma=(2.0, 3.0))), # move pixels locally around (with random strengths)
+            #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02), mode='constant')), # sometimes move parts of the image around
             sometimes(iaa.AdditiveGaussianNoise((0.02, 0.1))),
             sometimes(iaa.AdditivePoissonNoise((0.02,0.05)))
         ])
