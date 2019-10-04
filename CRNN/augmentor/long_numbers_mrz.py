cad87d551382a82170d0700bd7310b0643d8d7da
MichalLukac <cospelthetraceur@gmail.com>
2019-10-04 15:00:44 +0200

    Update augment for mrz

---
+++ CRNN/augmentor/long_numbers_mrz.py
@@ -0,0 +1,42 @@
+
+import imgaug as ia
+import imgaug.augmenters as iaa
+
+
+class MyAugmentor(object):
+    def __init__(self):
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+        self.seq = iaa.Sequential([
+            iaa.Multiply((0.8, 1.2), per_channel=0.5),
+            sometimes(
+                iaa.OneOf([
+                    iaa.CoarseDropout((0.01, 0.03), size_percent=(0.1, 0.3)),
+                    iaa.CoarseDropout((0.01, 0.03), size_percent=(0.1, 0.3), per_channel=1.0),
+                    iaa.Dropout((0.03,0.05)),
+                    iaa.Salt((0.03,0.05))
+                ])
+            ),
+            sometimes(iaa.FrequencyNoiseAlpha(
+                    exponent=(-4, 0),
+                    first=iaa.Multiply((0.8, 1.2), per_channel=0.5),
+                    second=iaa.ContrastNormalization((0.8, 1.5))
+                )
+            ),
+            sometimes(
+                iaa.OneOf([
+                    iaa.MotionBlur(k=(3,4),angle=(0, 360)),
+                    iaa.GaussianBlur((0, 1.2)),
+                    iaa.AverageBlur(k=(2, 3)),
+                    iaa.MedianBlur(k=(3, 5))
+                ])
+            ),
+            sometimes(
+                iaa.CropAndPad(
+                    percent=(-0.02, 0.02),
+                    pad_mode='constant',
+                    pad_cval=(0, 255)
+                ),
+            ),
+            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.1))),
+            sometimes(iaa.AdditivePoissonNoise((0.02,0.05)))
+        ])
