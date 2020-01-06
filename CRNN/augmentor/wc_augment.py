f904927db3222f7ff12d935cb651beac41c7abdb
MichalLukac <cospelthetraceur@gmail.com>
2020-01-06 13:23:52 +0100

    Update augmentation

--- CRNN/augmentor/wc_augment.py
+++ CRNN/augmentor/wc_augment.py
@@ -11,6 +11,16 @@ class MyAugmentor(object):
                 iaa.Multiply((0.6, 1.0), per_channel=0.5),
                 iaa.Multiply((1.0, 2.0), per_channel=0.5),
             ]),
+            sometimes(
+                iaa.OneOf([
+                    iaa.OneOf([
+                        iaa.CoarseDropout((0.03, 0.05), size_percent=(0.05, 0.1)),
+                        iaa.CoarseDropout((0.03, 0.05), size_percent=(0.05, 0.1), per_channel=1.0),
+                        iaa.Dropout((0.03, 0.05))
+                    ]),
+                    iaa.Salt((0.03, 0.2))
+                ])
+            ),
             sometimes(iaa.ChannelShuffle(1.0)),
             #sometimes(iaa.Invert(1.0, per_channel=True)),
             #sometimes(iaa.Invert(1.0)),
@@ -18,8 +28,7 @@ class MyAugmentor(object):
                 percent=(-0.15, 0.15), pad_cval=(0,255)
             )),
             sometimes(iaa.Affine(
-                scale={"x": (0.6, 1.2), "y": (0.6, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                #translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+                scale={"x": (0.7, 1.2), "y": (0.7, 1.2)}, # scale images to 80-120% of their size, individually per axis
                 rotate=(-10, 10), # rotate by -45 to +45 degrees
                 shear=(-5, 5), # shear by -16 to +16 degrees
                 order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
