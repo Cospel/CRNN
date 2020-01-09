7592e47880630721f66237351e037cf986899278
MichalLukac <cospelthetraceur@gmail.com>
2020-01-09 13:24:43 +0100

    Update augmentor for wc

--- CRNN/augmentor/wc_augment.py
+++ CRNN/augmentor/wc_augment.py
@@ -13,29 +13,25 @@ class MyAugmentor(object):
             ]),
             sometimes(
                 iaa.OneOf([
-                    iaa.OneOf([
-                        iaa.CoarseDropout((0.03, 0.05), size_percent=(0.05, 0.1)),
-                        iaa.CoarseDropout((0.03, 0.05), size_percent=(0.05, 0.1), per_channel=1.0),
-                        iaa.Dropout((0.03, 0.05))
-                    ]),
-                    iaa.Salt((0.03, 0.2))
+                    iaa.Dropout((0.02, 0.03)),
+                    iaa.Salt((0.02, 0.03))
                 ])
             ),
             sometimes(iaa.ChannelShuffle(1.0)),
             #sometimes(iaa.Invert(1.0, per_channel=True)),
             #sometimes(iaa.Invert(1.0)),
             sometimes(iaa.CropAndPad(
-                percent=(-0.15, 0.15), pad_cval=(0,255)
+                percent=(-0.1, 0.1), pad_cval=(0,255)
             )),
-            sometimes(iaa.Affine(
-                scale={"x": (0.7, 1.2), "y": (0.7, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                rotate=(-10, 10), # rotate by -45 to +45 degrees
-                shear=(-5, 5), # shear by -16 to +16 degrees
-                order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-                cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+            iaa.Affine(
+                #scale={"x": (0.8, 1.1), "y": (0.8, 1.1)}, # scale images to 80-120% of their size, individually per axis
+                rotate=(-15, 15), # rotate by -45 to +45 degrees
                 mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-            )),
+            ),
             sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.08))),
-            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.2))),
-            sometimes(iaa.AdditivePoissonNoise((0.02,0.1)))
+            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.1))),
+            sometimes(iaa.AdditivePoissonNoise((0.02,0.1))),
+            sometimes(iaa.Pad(
+                percent=(0, 0.15), pad_mode=["edge"]
+            ))
         ])
