906ef3b31c318df74770ab1205c2efdc560a2ece
MichalLukac <cospelthetraceur@gmail.com>
2020-01-16 15:12:00 +0100

    Update augmentor

--- CRNN/augmentor/wc_augment.py
+++ CRNN/augmentor/wc_augment.py
@@ -20,18 +20,18 @@ class MyAugmentor(object):
             sometimes(iaa.ChannelShuffle(1.0)),
             #sometimes(iaa.Invert(1.0, per_channel=True)),
             #sometimes(iaa.Invert(1.0)),
-            sometimes(iaa.CropAndPad(
-                percent=(-0.1, 0.1), pad_cval=(0,255)
-            )),
+            #sometimes(iaa.CropAndPad(
+            #    percent=(-0.1, 0.1), pad_cval=(0,255)
+            #)),
             iaa.Affine(
                 #scale={"x": (0.8, 1.1), "y": (0.8, 1.1)}, # scale images to 80-120% of their size, individually per axis
-                rotate=(-15, 15), # rotate by -45 to +45 degrees
+                rotate=(-10, 10), # rotate by -45 to +45 degrees
                 mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
             ),
-            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.08))),
+            #sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.08))),
             sometimes(iaa.AdditiveGaussianNoise((0.02, 0.1))),
             sometimes(iaa.AdditivePoissonNoise((0.02,0.1))),
-            sometimes(iaa.Pad(
-                percent=(0, 0.15), pad_mode=["edge"]
-            ))
+            #sometimes(iaa.Pad(
+            #    percent=(0, 0.15), pad_mode=["edge"]
+            #))
         ])
